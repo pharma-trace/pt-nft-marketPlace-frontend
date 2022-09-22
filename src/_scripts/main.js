289374ce5c980d86395df40b3e501d545e4188c2
federicolaterra <f.laterra@pharmatrace.io>
2022-09-22 17:01:09 +0200

    initial commit

---
+++ src/_scripts/main.js
@@ -0,0 +1,12 @@
+// Main javascript entry point
+// Should handle bootstrapping/starting application
+
+'use strict';
+
+import 'core-js';
+import 'regenerator-runtime/runtime';
+
+(function() {
+  console.log('Ready');
+})();
+
