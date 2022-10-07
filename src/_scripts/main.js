a46acabf7de128bf4a58192fb06d6600961de44f
federicolaterra <f.laterra@pharmatrace.io>
2022-10-07 10:54:55 +0200

    fixed (variants card-collection) and added pages ([privacy policy, cookie policy, terms of service])

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -7,8 +7,17 @@ import 'core-js';
 import 'regenerator-runtime/runtime';
 import './tailwind.config';
 
+import ActiveMenuLink from 'active-menu-link';
+
 (function() {
   console.log('Ready');
   //document.querySelector('input[name = "category"]:checked').value;
+
+  let options = {
+    default: 'paragraph-1',
+    showHash: false
+  };
+
+  new ActiveMenuLink('.nft__active-menu-link', options);
 })();
 
