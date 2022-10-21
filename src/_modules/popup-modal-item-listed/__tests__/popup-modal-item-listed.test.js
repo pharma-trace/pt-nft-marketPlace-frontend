654025b77dd2565b3bd75129c482d228da9dcf13
federicolaterra <f.laterra@pharmatrace.io>
2022-10-21 10:59:56 +0200

    added popup-modal-item-listed

---
+++ src/_modules/popup-modal-item-listed/__tests__/popup-modal-item-listed.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import PopupModalItemListed from '../popup-modal-item-listed';
+
+describe('PopupModalItemListed View', function() {
+
+  beforeEach(() => {
+    this.popupModalItemListed = new PopupModalItemListed();
+  });
+
+  it('Should run a few assertions', () => {
+    expect(this.popupModalItemListed);
+  });
+
+});
---
+++ src/_modules/popup-modal-item-listed/popup-modal-item-listed.js
@@ -0,0 +1,7 @@
+'use strict';
+
+export default class PopupModalItemListed {
+  constructor() {
+    this.name = 'popup-modal-item-listed';
+  }
+}
