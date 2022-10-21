40a5eb26fc99b7154f5b9a675d3cd29ef19d4ae0
federicolaterra <f.laterra@pharmatrace.io>
2022-10-21 17:26:32 +0200

    added page how-it-works

---
+++ src/_modules/illustration-what-is-nft-svg/__tests__/illustration-what-is-nft-svg.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import IllustrationWhatIsNftSvg from '../illustration-what-is-nft-svg';
+
+describe('IllustrationWhatIsNftSvg View', function() {
+
+  beforeEach(() => {
+    this.illustrationWhatIsNftSvg = new IllustrationWhatIsNftSvg();
+  });
+
+  it('Should run a few assertions', () => {
+    expect(this.illustrationWhatIsNftSvg);
+  });
+
+});
---
+++ src/_modules/illustration-what-is-nft-svg/illustration-what-is-nft-svg.js
@@ -0,0 +1,7 @@
+'use strict';
+
+export default class IllustrationWhatIsNftSvg {
+  constructor() {
+    this.name = 'illustration-what-is-nft-svg';
+  }
+}
